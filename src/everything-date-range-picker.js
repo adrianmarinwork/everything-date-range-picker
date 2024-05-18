fa1c33982523988d002576d996cc51233a859921
adrianmarinwork <adrianmarinwork@gmail.com>
2024-05-18 19:43:26 +0200

    Setting properties and click events to show and hide calendar

--- src/everything-date-range-picker.js
+++ src/everything-date-range-picker.js
@@ -1,6 +1,29 @@
 // import "./everything-date-range-picker.css";
 
 class EverythingDateRangePicker {
+  #defaultRanges = [
+    { label: "Today", startDate: "", endDate: "" },
+    { label: "Yesterday", startDate: "", endDate: "" },
+    { label: "Last 7 Days", startDate: "", endDate: "" },
+    { label: "Last 30 Days", startDate: "", endDate: "" },
+    { label: "This Week", startDate: "", endDate: "" },
+    { label: "Last Week", startDate: "", endDate: "" },
+    { label: "This Month", startDate: "", endDate: "" },
+    { label: "Last Month", startDate: "", endDate: "" },
+    { label: "Year to Date", startDate: "", endDate: "" },
+    { label: "Last Year", startDate: "", endDate: "" },
+  ];
+
+  #granularitiesAvailable = [
+    "hours",
+    "days",
+    "weeks",
+    "months",
+    "quarters",
+    "semesters",
+    "years",
+  ];
+
   constructor(containerId, options = {}) {
     this.container = document.getElementById(containerId);
 
@@ -8,8 +31,17 @@ class EverythingDateRangePicker {
     console.log("this.container: ", this.container);
     console.log("options: ", options);
 
-    this.selectedStartDate = options.selectedStartDate || null;
-    this.selectedEndDate = options.selectedEndDate || null;
+    this.startDate = options.startDate || null;
+    this.endDate = options.endDate || null;
+    this.minDate = options.minDate || null;
+    this.maxDate = options.maxDate || null;
+    this.timezone = options.timezone || null;
+    this.showTimezoneChooser = options.showTimezoneChooser || false;
+    this.granularity = options.granularity || "hours";
+    this.singleCalendar = options.singleCalendar || false;
+    this.ranges = options.ranges || this.#defaultRanges;
+    this.spanOfSelecteableDays = options.spanOfSelecteableDays || null;
+    this.firstDayOfWeek = options.firstDayOfWeek || "Monday";
 
     this.initDatePicker();
   }
@@ -24,7 +56,7 @@ class EverythingDateRangePicker {
         </div>
         <div class="date-range-picker-calendar-container">
           <div class="calendar start-date-calendar"> test </div>
-          <div class="calendar end-date-calendar"> test</div>
+          <div class="calendar end-date-calendar"> test </div>
         </div>
       </div>
     `;
@@ -34,6 +66,30 @@ class EverythingDateRangePicker {
       ".date-range-picker-calendar-container"
     );
     this.selectedDatesElement = this.container.querySelector(".selected-dates");
+
+    this.display.addEventListener("click", () => this.toggleCalendar());
+    document.addEventListener("click", (event) =>
+      this.documentClickHandler(event)
+    );
+  }
+
+  /**
+   * Method to hide or show the calendar when clicking the container of .date-range-picker-display
+   */
+  toggleCalendar() {
+    const currentDisplay = this.calendarContainer.style.display;
+    this.calendarContainer.style.display =
+      currentDisplay === "flex" ? "none" : "flex";
+  }
+
+  /**
+   * Method to hide the calendar when clicking anywhere in the screen except the date range picker
+   * @param {Object} event The click object that contains the info of the event
+   */
+  documentClickHandler(event) {
+    if (!this.container.contains(event.target)) {
+      this.calendarContainer.style.display = "none";
+    }
   }
 }
 
